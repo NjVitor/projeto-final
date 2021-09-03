abeaeecdb298222ff828d2bc4c687d73d7f7bd34
NjVitor <njosevitor174@gmail.com>
2021-09-03 20:36:33 -0300

    03/09

---
+++ Projeto Final/js/script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+    $('.gallery-001').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery:{enabled:true}
+        // other options
+      });
+});
